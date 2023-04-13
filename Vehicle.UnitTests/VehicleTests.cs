8416c33f8e7419814672d392dd29acca707ce5e9
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-04-13 10:07:38 -0600

    Eli's Finished Tests

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -17,12 +17,32 @@ namespace Vehicle.UnitTests
         public void Summary_ReturnsInfoAboutVehicle()
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
-
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
-
+            string summary = andysBike.Sumary();
+            var expected = "This Blue vehicle has 2 wheels, and has driven 0 miles.";
+            Assert.Equal(expected, summary);
 
         }
 
         // Add more tests here!
+
+        [Fact]
+        public void Check_If_DriveMethod_AddsInt()
+        {
+            Vehicle car = new Vehicle(3, "lemon");
+            car.Drive();
+            int expected = 5;
+            Assert.Equal(expected, car.MilesDriven);
+            
+        }
+
+        [Fact]
+        public void Check_If_PaintMethod_CanChange_CarColor()
+        {
+            Vehicle car = new Vehicle(3, "lemon");
+            car.Paint("a really ugly beige color");
+            string expected = "a really ugly beige color";
+            Assert.Equal(expected, car.Color);
+
+        }
     }
 }
